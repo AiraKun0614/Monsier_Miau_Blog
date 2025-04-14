e625dc0c3caa2bbd10547bfccba60b0e7a210b7e
Isai Herrera <isai.herrera@utp.ac.pa>
2025-04-13 21:34:34 -0500

    Proyecto Base
    Archivos que entrego el profesor + Sistema de autenticación de usuarios

---
+++ blogapp/urls.py
@@ -0,0 +1,16 @@
+from django.urls import path
+from .views import BlogListView, BlogDetailView, ReviewCreateView, CommentCreateView, BlogCreateView, register, user_login, user_logout
+
+app_name = 'blogapp'
+
+
+urlpatterns = [
+    path('', BlogListView.as_view(), name='blog_list'),
+    path('blog/add/', BlogCreateView.as_view(), name='add_blog'),
+    path('blog/<int:pk>/', BlogDetailView.as_view(), name='blog_detail'),
+    path('blog/<int:pk>/review/', ReviewCreateView.as_view(), name='add_review'),
+    path('blog/<int:blog_pk>/review/<int:review_pk>/comment/', CommentCreateView.as_view(), name='add_comment'),
+    path('register/', register, name='register'),
+    path('login/', user_login, name='login'),
+    path('logout/', user_logout, name='logout'),
+]
